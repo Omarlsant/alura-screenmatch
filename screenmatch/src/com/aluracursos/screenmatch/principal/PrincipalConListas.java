b0243cc37d4dbab9b895d2cb1a3d7e544520e39c
Omar Lengua <omarns21@gmail.com>
2025-05-27 22:50:06 +0200

    Implement: adding method and cast Pelicula constructor.

--- screenmatch/src/com/aluracursos/screenmatch/principal/PrincipalConListas.java
+++ screenmatch/src/com/aluracursos/screenmatch/principal/PrincipalConListas.java
@@ -9,8 +9,11 @@ import java.util.ArrayList;
 public class PrincipalConListas {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula("Encanto", 2021);
+        miPelicula.evalua(9);
         Pelicula otraPelicula = new Pelicula("Avatar", 2023);
+        otraPelicula.evalua(6);
         var peliculaDeOmar = new Pelicula("El señor de los anillos", 2001);
+        peliculaDeOmar.evalua(10);
 
         Serie lost = new Serie("Lost", 2000);
 
@@ -19,5 +22,11 @@ public class PrincipalConListas {
         lista.add(miPelicula);
         lista.add(otraPelicula);
         lista.add(lost);
+
+        for (Titulo item: lista) {
+            System.out.println(item);
+            Pelicula pelicula = (Pelicula)item;
+            System.out.println(((Pelicula) item).getClasificacion());
+        }
     }
 }
